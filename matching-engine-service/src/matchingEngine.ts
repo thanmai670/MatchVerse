a613acd5c754dd342e6ff36d29f021c7ea3fef6d
Thanmai <thanmaibk@genie-enterprise.com>
2024-09-28 01:09:13 +0200

    replicate integration

--- matching-engine-service/src/matchingEngine.ts
+++ matching-engine-service/src/matchingEngine.ts
@@ -2,12 +2,17 @@ import { Ollama } from 'ollama';
 import dotenv from 'dotenv';
 import { MatchResult, JobData, ResumeData } from './shared/messages';
 import { publishMatchResult } from './redisClient';
+import Replicate from "replicate";
 
 dotenv.config();
 
-const ollama = new Ollama({
-  host: process.env.OLLAMA_BASE_URL || 'http://ollama-service:11434',
-});
+// set REPLICATE_API_TOKEN as my environment variable
+
+// const ollama = new Ollama({
+//   host: process.env.OLLAMA_BASE_URL || 'http://ollama-service:11434',
+// });
+
+const replicate = new Replicate();
 
 export const matchJobAndResume = async (jobData: JobData, resumeData: ResumeData) => {
   console.log('Matching job and resume preparing prompt');
@@ -23,12 +28,14 @@ export const matchJobAndResume = async (jobData: JobData, resumeData: ResumeData
     Provide the score and reasoning in JSON format like {"score": 0, "reasoning": "..."}.
   `;
 
-  const res = await ollama.generate({model:'llama3.1',
-   prompt:prompt
-  });
 
-  const response = JSON.parse(res.response);
-  console.log('Match result received', response);
+
+  // const res = await ollama.generate({model:'llama3.1',
+  //  prompt:prompt
+  // });
+
+  // const response = JSON.parse(res.response);
+  // console.log('Match result received', response);
 
   // const result = JSON.parse(response.message.content) as MatchResult;
 
@@ -37,5 +44,12 @@ export const matchJobAndResume = async (jobData: JobData, resumeData: ResumeData
 
   // console.log('Match result received');
 
-  await publishMatchResult(response);
+  const input = {
+    prompt: prompt,
+    max_tokens: 1024,
+  };
+
+  const output = await replicate.run("meta/meta-llama-3.1-405b-instruct", { input });
+
+  await publishMatchResult(output);
 };
